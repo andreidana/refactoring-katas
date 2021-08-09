42a07c49677c059f214c099e56b998c9258ac304
Andrei Dana <andrei.dana87@gmail.com>
2021-08-09 23:16:41 +0300

    Tennis kata - refactor first class code

--- Tennis/FirstTennisImplementation/TennisGame.cs
+++ Tennis/FirstTennisImplementation/TennisGame.cs
@@ -1,84 +1,102 @@
+using System;
+using System.Collections.Generic;
 using Tennis.Contract;
 
 namespace Tennis.First
 {
     public class TennisGame : ITennisGame
     {
-        private int m_score1 = 0;
-        private int m_score2 = 0;
-        private string player1Name;
-        private string player2Name;
+        private const string DEUCE = "Deuce";
+        private int _firstPlayerScore;
+        private int _secondPlayerScore;
+        private string _player1Name;
+        private string _player2Name;
 
         public TennisGame(string player1Name, string player2Name)
         {
-            this.player1Name = player1Name;
-            this.player2Name = player2Name;
+            this._player1Name = player1Name;
+            this._player2Name = player2Name;
         }
 
         public void WonPoint(string playerName)
         {
             if (playerName == "player1")
-                m_score1 += 1;
+                _firstPlayerScore++;
             else
-                m_score2 += 1;
+                _secondPlayerScore++;
         }
 
         public string GetScore()
         {
-            string score = "";
-            var tempScore = 0;
-            if (m_score1 == m_score2)
+            var score = string.Empty;
+            if (_firstPlayerScore == _secondPlayerScore)
             {
-                switch (m_score1)
-                {
-                    case 0:
-                        score = "Love-All";
-                        break;
-                    case 1:
-                        score = "Fifteen-All";
-                        break;
-                    case 2:
-                        score = "Thirty-All";
-                        break;
-                    default:
-                        score = "Deuce";
-                        break;
-
-                }
+                score = GetEqualityScore();
             }
-            else if (m_score1 >= 4 || m_score2 >= 4)
+            else if (_firstPlayerScore >= 4 || _secondPlayerScore >= 4)
             {
-                var minusResult = m_score1 - m_score2;
-                if (minusResult == 1) score = "Advantage player1";
-                else if (minusResult == -1) score = "Advantage player2";
-                else if (minusResult >= 2) score = "Win for player1";
-                else score = "Win for player2";
+                score = GetAdvantageScore();
             }
             else
             {
-                for (var i = 1; i < 3; i++)
-                {
-                    if (i == 1) tempScore = m_score1;
-                    else { score += "-"; tempScore = m_score2; }
-                    switch (tempScore)
-                    {
-                        case 0:
-                            score += "Love";
-                            break;
-                        case 1:
-                            score += "Fifteen";
-                            break;
-                        case 2:
-                            score += "Thirty";
-                            break;
-                        case 3:
-                            score += "Forty";
-                            break;
-                    }
-                }
+                score = GetNormalScore(score);
             }
+
             return score;
+        }
+
+        private string GetNormalScore(string score)
+        {
+            var possibleScores = new Dictionary<int, string>()
+            {
+                {0, "Love"},
+                {1, "Fifteen"},
+                {2, "Thirty"},
+                {3, "Forty"}
+            };
+
+            for (var i = 1; i < 3; i++)
+            {
+                int tempScore;
+                if (i == 1)
+                {
+                    tempScore = _firstPlayerScore;
+                }
+                else
+                {
+                    score += "-";
+                    tempScore = _secondPlayerScore;
+                }
+
+                possibleScores.TryGetValue(tempScore, out var parsedScore);
+                score += parsedScore;
+            }
+
+            return score;
+        }
+
+        private string GetAdvantageScore()
+        {
+            var advantageScores = new Dictionary<int, string>()
+            {
+                {1, "Advantage player1"},
+                {-1, "Advantage player2"}
+            };
+
+            var minusResult = _firstPlayerScore - _secondPlayerScore;
+            return advantageScores.GetValueOrDefault(minusResult, minusResult >= 2 ? "Win for player1" : "Win for player2");
+        }
+
+        private string GetEqualityScore()
+        {
+            var possibleEqualityScores = new Dictionary<int, string>()
+            {
+                {0, "Love-All"},
+                {1, "Fifteen-All"},
+                {2, "Thirty-All"}
+            };
+
+            return possibleEqualityScores.GetValueOrDefault(_firstPlayerScore, DEUCE);
         }
     }
 }
-
